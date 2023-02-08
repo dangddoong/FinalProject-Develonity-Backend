efca8f1d034be5980c03eb5596167acc16f1f8bb
SungWonSong <sungone34@gmail.com>
2023-02-08 14:24:30 +0900

    2-1 [feat] user 기본 세팅

--- src/main/java/com/develonity/user/entity/User.java
+++ src/main/java/com/develonity/user/entity/User.java
@@ -1,5 +1,13 @@
 package com.develonity.user.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity(name="users")
 public class User {
 
 }
