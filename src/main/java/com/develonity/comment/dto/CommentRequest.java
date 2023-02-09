3b97beed88e74a0eb352fc0639e5855628ed39f7
jiho <shl05234@naver.com>
2023-02-09 10:01:37 +0900

    5-2 [feat] - 댓글 조회 기능, 댓글 CRUD 기능 추가(약식)

---
+++ src/main/java/com/develonity/comment/dto/CommentRequest.java
@@ -0,0 +1,9 @@
+package com.develonity.comment.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CommentRequest {
+
+  public String content;
+}
--- src/main/java/com/develonity/comment/service/CommentService.java
+++ src/main/java/com/develonity/comment/service/CommentService.java
@@ -1,5 +1,20 @@
 package com.develonity.comment.service;
 
+import com.develonity.comment.dto.CommentRequest;
+import com.develonity.comment.dto.CommentResponse;
+import com.develonity.user.entity.User;
+import java.util.List;
+import org.springframework.data.domain.Page;
+
 public interface CommentService {
 
+  Page<CommentResponse> getAllComment(int page, int size);
+
+  List<CommentResponse> getMyComments(int page, int size, Long userId,
+      User user);
+
+  CommentResponse createQuestionComment(Long boardId, CommentRequest requestDto, User user);
+
+  CommentResponse updateQuestionComment(Long boardId, Long commentId, CommentRequest request,
+      User user);
 }
