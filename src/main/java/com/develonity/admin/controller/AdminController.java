c60d505e846188fe0fa0c2b296fee636a4da27dd
SungWonSong <sungone34@gmail.com>
2023-02-09 23:48:37 +0900

    6. admin 기본세팅

--- src/main/java/com/develonity/admin/controller/AdminController.java
+++ src/main/java/com/develonity/admin/controller/AdminController.java
@@ -1,5 +1,12 @@
 package com.develonity.admin.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.RestController;
+
+@Getter
+@AllArgsConstructor
+@RestController
 public class AdminController {
 
 }
---
+++ src/main/java/com/develonity/admin/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.develonity.admin.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long>{
+
+}
+
