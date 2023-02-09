dc6a266da7cbd519652071662972b84077c817c1
SungWonSong <sungone34@gmail.com>
2023-02-10 03:10:19 +0900

    6-3 <setting> 유저 전체 댓글 조회 / 수정

--- src/main/java/com/develonity/admin/controller/AdminController.java
+++ src/main/java/com/develonity/admin/controller/AdminController.java
@@ -2,12 +2,15 @@ package com.develonity.admin.controller;
 
 import com.develonity.admin.repository.AdminRepository;
 import com.develonity.admin.service.AdminService;
+import com.develonity.comment.dto.CommentResponse;
 import com.develonity.user.dto.RegisterRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,9 +25,9 @@ public class AdminController {
     private final AdminRepository adminRepository;
     private final AdminService adminService;
 
-    // 전체회원 조회(아이디, 실명, 닉네임) -  dTO를 User에서 받아오는게 맞지않나?
-    @GetMapping("/register")
-    public List<RegisterResponse> findRegisterList(){
+    // 전체회원 조회(아이디, 실명, 닉네임) -  dTO를 User에서
+    @GetMapping("/register")// 명사로 registration / enrollement
+    public List<LoginResponse> findRegisterList(){
         return adminService.findRegisterList();
     }
 
@@ -34,4 +37,16 @@ public class AdminController {
         adminService.findRegisterinfo();
     }
 
+    // 어드민이 유저의 전체댓글 조회(admin입장에서 필요하다고 생각)
+    @GetMapping ("/register/{id}/comments")
+    public List<CommentResponse> findRegisterComments(){adminService.findRegisterComments();}
+
+    // 어드민 유저 전체 댓글 중 댓글수정....
+    @PutMapping("/register/{id}/comments/updates")
+    public List<CommentResponse> updateRegisterComments(){adminService.updateRegisterComments();}
+    public
+
+
+    //order목록, 상품목록 추가 (관련 로직 설정 필요)
+    // @PutMapping("/order")
 }
--- src/main/java/com/develonity/admin/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.develonity.admin.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin, Long>{
-
-}
-
