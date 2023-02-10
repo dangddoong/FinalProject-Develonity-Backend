bd10cd2751e8a3c6481f88af415c646a732ec5e8
SungWonSong <sungone34@gmail.com>
2023-02-11 03:00:26 +0900

    6-3 <feat> Admin controller / serviceimpl 구현중

--- src/main/java/com/develonity/admin/controller/AdminController.java
+++ src/main/java/com/develonity/admin/controller/AdminController.java
@@ -25,17 +25,16 @@ public class AdminController {
 
     // 전체회원 조회(아이디, 실명, 닉네임, 롤) -  dTO를 User에서
     @GetMapping("/users")
-    public List<userResponse> findRegisterList(){
-        return adminService.findRegisterList();
-    }
+    public List<userResponse> findUsersList(){return adminService.findUserList();}
 
     // 개인회원 조회(아이디, 실명, 닉네임, 비밀번호) 회원가입내역 전체조회로 Dto
     @GetMapping("/users/{id}")
-    public List<RegisterResponse> findRegisterinfo(){return adminService.findRegisterinfo();}
+    public List<RegisterResponse> findUsersinfo(){return adminService.findUsersinfo();}
 
     // 어드민이 유저의 전체댓글 조회(admin입장에서 필요하다고 생각)
     @GetMapping ("/users/{id}/comments")
     public List<CommentResponse> findRegisterComments(){return adminService.findRegisterComments();}
+
 
     // 어드민 유저 전체 댓글 삭제....
     //@DeleteMapping("/register/{id}/comments/delete")
