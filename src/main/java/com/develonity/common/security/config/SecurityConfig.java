a9f923c486489a9dbfae585b73a4b06f66354690
dangddoong <dangddoong@gmail.com>
2023-02-10 11:20:13 +0900

    1-3 [fix] h2 DB와 회원가입&로그인 API 사용할 수 있도록 Path 열었습니다.

--- src/main/java/com/develonity/common/security/config/SecurityConfig.java
+++ src/main/java/com/develonity/common/security/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import com.develonity.common.jwt.JwtUtil;
 import com.develonity.common.security.exceptionHandler.AccessDeniedHandlerImpl;
 import com.develonity.common.security.exceptionHandler.AuthenticationEntryPointImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,7 +33,15 @@ public class SecurityConfig {
   }
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring()
+        .requestMatchers(PathRequest.toH2Console())
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
+
+
+  @Bean
+  protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
     http.httpBasic().disable()
         .csrf().disable()
         .formLogin().disable();
@@ -40,9 +50,8 @@ public class SecurityConfig {
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션이 필요하면 생성하도록 셋팅
 
     http.authorizeHttpRequests()
-        .antMatchers("/h2-console/**").permitAll()
-//        .antMatchers("/api/users/signin").permitAll()
-//        .antMatchers("/api/users/signup").permitAll()
+        .antMatchers("/api/register").permitAll()
+        .antMatchers("/api/login").permitAll()
 //        .antMatchers("/api/users/signout").permitAll()
 //        .antMatchers("/api/users/**").hasRole("CUSTOMER")
 //        .antMatchers("/api/sellers/**").hasRole("SELLER")
