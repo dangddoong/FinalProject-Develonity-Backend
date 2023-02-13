0f5636818774eb5b30a928bbe3563a32ac8b1c3d
jiho <shl05234@naver.com>
2023-02-13 12:36:33 +0900

    5-3 [feat] - 댓글 CRUD 기능 구현 / 질문/잡담 댓글 분리 / 좋아요 기능 미구현..

---
+++ src/main/java/com/develonity/comment/dto/CommentList.java
@@ -0,0 +1,28 @@
+package com.develonity.comment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Getter
+@Setter
+public class CommentList {
+
+  private int page;
+  private int size;
+  private boolean isAsc;
+
+  public Pageable toPageable() {
+    page -= 1;
+    page = Math.max(page, 0);
+    Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+//    Sort sort = Sort.by(Sort.Direction.DESC, "id");
+    Sort sort = Sort.by(direction, "id");
+
+    return PageRequest.of(page, size, sort);
+  }
+
+
+}
--- src/main/java/com/develonity/comment/service/CommentServiceImpl.java
+++ src/main/java/com/develonity/comment/service/CommentServiceImpl.java
@@ -1,19 +1,13 @@
 package com.develonity.comment.service;
 
+import com.develonity.comment.dto.CommentList;
 import com.develonity.comment.dto.CommentRequest;
 import com.develonity.comment.dto.CommentResponse;
 import com.develonity.comment.entity.Comment;
 import com.develonity.comment.repository.CommentRepository;
 import com.develonity.user.entity.User;
-import com.develonity.user.entity.UserRole;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +17,9 @@ public class CommentServiceImpl implements CommentService {
 
   private final CommentRepository commentRepository;
 
+  private final CommentLikeService commentLikeService;
+
+
   // 댓글이 있는지 확인하는 기능
   private Comment getComment(Long commentId) {
     return commentRepository.findById(commentId).orElseThrow(
@@ -30,64 +27,58 @@ public class CommentServiceImpl implements CommentService {
     );
   }
 
-  // 작성자와 현재 유저가 같은지, 어드민인지 확인
+  // 작성자와 현재 유저가 같은지 확인하는 기능
   private void checkUser(User user, Comment comment) {
-    if (comment.getUsername() != user.getNickName() && user.getUserRole() != UserRole.ADMIN) {
-      throw new IllegalStateException("댓글 작성자가 아니거나 권한이 없습니다.");
+    if (comment.getNickName() != user.getNickName()) {
+      throw new IllegalStateException("댓글 작성자가 아닙니다.");
     }
   }
 
   // 전체 댓글 조회
   @Override
   @Transactional(readOnly = true)
-  public Page<CommentResponse> getAllComment(int page, int size) {
+  public Page<CommentResponse> getAllComment(User user, CommentList commentList) {
     // 페이징 처리
-    Sort sort = Sort.by(Direction.DESC, "id");
-    Pageable pageable = PageRequest.of(page, size, sort);
-
-    Page<Comment> comments = commentRepository.findAll(pageable);
-    Page<CommentResponse> commentResponseDto = comments.map(
-        CommentResponse::toCommentResponseDto);
-    return commentResponseDto;
+    Page<Comment> commentPages = commentRepository.findBy(commentList.toPageable());
+    return commentPages.map(
+        comment -> new CommentResponse(comment, commentLikeService.addLike((comment.getId()))));
   }
 
-  // 내가 쓴 댓글 전체 조회
+  // 내가 쓴 댓글 전체 조회 (페이징 처리)
   @Override
   @Transactional(readOnly = true)
-  public List<CommentResponse> getMyComments(int page, int size, Long userId, User user) {
+  public Page<CommentResponse> getMyComments(CommentList commentList, Long userId, User user) {
 
     if (userId != user.getId()) {
       throw new IllegalStateException("권한이 없습니다");
     }
-    Sort sort = Sort.by(Direction.DESC, "id");
-    Pageable pageable = PageRequest.of(page, size, sort);
 
-    Page<Comment> myCommentList = commentRepository.findAllByUsername(pageable, user.getNickName());
-    List<CommentResponse> myCommentListDto = myCommentList.getContent()
-        .stream()
-        .map(CommentResponse::new)
-        .collect(Collectors.toList());
-
-    return myCommentListDto;
+    Page<Comment> myCommentList = commentRepository.findAllByNickName(commentList.toPageable(),
+        user.getNickName());
+    return myCommentList.map(
+        comment1 -> new CommentResponse(comment1, commentLikeService.addLike(comment1.getId())));
   }
 
   // 질문게시글 답변 작성
   @Override
   @Transactional
-  public void createQuestionComment(Long boardId, CommentRequest requestDto,
+  public CommentResponse createQuestionComment(Long questionBoardId, CommentRequest requestDto,
       User user) {
     // 게시물이 있는지 확인
-//    Board board = boardService.getBoard(boardId);
+//    Board board = boardRepository.findById(boardid).orElseThrow(
+//        () -> new IllegalArgumentException("게시물이 없습니다.")
+//    );
+
     // 댓글 생성
     Comment comment = new Comment(user, requestDto);
     commentRepository.save(comment);
-    new CommentResponse(comment);
+    return new CommentResponse(comment, commentLikeService.addLike(comment.getId()));
   }
 
   // 질문 게시글 수정
   @Override
   @Transactional
-  public void updateQuestionComment(Long boardId, Long commentId,
+  public void updateQuestionComment(Long questionBoardId, Long commentId,
       CommentRequest request,
       User user) {
     // 게시물이 있는지 확인
@@ -96,29 +87,27 @@ public class CommentServiceImpl implements CommentService {
     Comment comment = getComment(commentId);
 
     // 권한 확인
-    // 댓글 작성자와 수정하려는 유저 닉네임이 같지 않거나, 어드민이 아니면 익셉션 출력
+    // 댓글 작성자와 수정하려는 유저 닉네임이 같지 않으면 익셉션 출력
     checkUser(user, comment);
-    // 댓글 작성자이거나 어드민인 경우 댓글 수정
+    // 댓글 작성자이면 댓글 수정
     comment.update(request.getContent());
     commentRepository.save(comment);
-    new CommentResponse(comment);
+    new CommentResponse(comment, commentLikeService.addLike(comment.getId()));
 
   }
 
   // 질문 댓글 삭제 기능
   @Override
   @Transactional
-  public void deleteQuestionComment(Long boardId, Long commentId,
-      User user) {
-    // 게시물이 있는지 확인
-//    Board board = boardService.getBoard(boardId);
+  public void deleteQuestionComment(Long commentId, User user) {
+
     // 댓글이 있는지 확인
     Comment comment = getComment(commentId);
 
     // 권한 확인
-    // 댓글 작성자와 수정하려는 유저 닉네임이 같지 않거나, 어드민이 아니면 익셉션 출력
+    // 댓글 작성자와 수정하려는 유저 닉네임이 같지 않으면 익셉션 출력
     checkUser(user, comment);
-    // 댓글 작성자이거나 어드민이면 댓글 삭제
+    // 댓글 작성자면 댓글 삭제
     commentRepository.delete(comment);
 
 
@@ -127,19 +116,20 @@ public class CommentServiceImpl implements CommentService {
   // 잡담게시글 댓글 작성
   @Override
   @Transactional
-  public void createCommunityComment(Long boardId, CommentRequest request, User user) {
+  public void createCommunityComment(Long communityBoardId, CommentRequest request, User user) {
     // 게시글이 있는지 확인
 //    Board board = boardService.getBoard(boardId);
 
     // 게시글이 있으면 댓글 작성
     Comment comment = new Comment(user, request);
     commentRepository.save(comment);
-    new CommentResponse(comment);
+    new CommentResponse(comment, commentLikeService.addLike(comment.getId()));
   }
 
+  // 잡담 댓글 수정
   @Override
   @Transactional
-  public void updateCommunityComment(Long boardId, Long commentId, CommentRequest request,
+  public void updateCommunityComment(Long communityBoardId, Long commentId, CommentRequest request,
       User user) {
     // 게시글이 있는지 확인
     // Board board = boardService.getBoard(boardId);
@@ -150,26 +140,24 @@ public class CommentServiceImpl implements CommentService {
     // 권한 확인
     // 댓글 작성자와 수정하려는 유저 닉네임이 같지 않거나, 어드민이 아니면 익셉션 출력
     checkUser(user, comment);
-    // 댓글 작성자이거나 어드민인 경우 댓글 수정
+    // 댓글 작성자인 경우 댓글 수정
     comment.update(request.getContent());
     commentRepository.save(comment);
-    new CommentResponse(comment);
+    new CommentResponse(comment, commentLikeService.addLike(comment.getId()));
 
   }
 
   @Override
   @Transactional
-  public void deleteCommunity(Long boardId, Long commentId, User user) {
-    // 게시글이 있는지 확인
-    // Board board = BoardService.getBoard(boardId),
+  public void deleteCommunity(Long commentId, User user) {
 
     // 댓글이 있는지 확인
     Comment comment = getComment(commentId);
 
     // 권한 확인
-    // 댓글 작성자와 수정하려는 유저 닉네임이 같지 않거나, 어드민이 아니면 익셉션 출력
+    // 댓글 작성자와 수정하려는 유저 닉네임이 같지 않으면 익셉션 출력
     checkUser(user, comment);
-    // 댓글 작성자이거나 어드민이면 댓글 삭제
+    // 댓글 작성자이면 댓글 삭제
     commentRepository.delete(comment);
   }
 }
