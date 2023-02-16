c8af6f029b111ee00d537a6c713cbf7bac8359b9
SungWonSong <sungone34@gmail.com>
2023-02-16 15:23:21 +0900

    6-8 admin 전체조회, 상세조회, 역할에 따른 조회 potman 정상작동 확인 추후 - UserResponsePage Dto로 리팩토링 필요

--- src/main/java/com/develonity/admin/controller/AdminController.java
+++ src/main/java/com/develonity/admin/controller/AdminController.java
@@ -1,40 +1,48 @@
 package com.develonity.admin.controller;
 
+import com.develonity.admin.dto.RegisterResponse;
+import com.develonity.admin.dto.UserResponse;
+import com.develonity.admin.repository.AdminUserRepository;
 import com.develonity.admin.service.AdminService;
-import com.develonity.user.repository.UserRepository;
+import com.develonity.common.security.users.UserDetailsImpl;
+import com.develonity.user.entity.User;
+import com.develonity.user.entity.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+
+
 
 @Getter
 @AllArgsConstructor
-@RestController("/api/admins")
+@RestController
+@RequestMapping("/api/admins")
 //admin이 한명이아니라 한번 생각
 @PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
 
-  private final UserRepository userRepository;
+  private final AdminUserRepository adminRepository;
   private final AdminService adminService;
 
   // 전체회원 조회(아이디, 실명, 닉네임, 롤) -  dTO를 User에서
-//    @GetMapping("/users")
-//    public List<userResponse> findUsersList(){return adminService.findUserList();}
+  @GetMapping("/users")
+  public Page<UserResponse> getAllUsers(@AuthenticationPrincipal UserDetailsImpl userDetails){
+    return adminService.getAllUsers(userDetails.getUser());}
+
 
   // 개인회원 조회(아이디, 실명, 닉네임, 비밀번호) 회원가입내역 전체조회로 Dto
-//    @GetMapping("/users/{id}")
-//    public List<RegisterResponse> findUsersinfo(){return adminService.findUsersinfo();}
+  @GetMapping("/users/{id}")
+  public RegisterResponse getUsersinfo(@PathVariable Long id, User user){
+    return adminService.getUsersInfo(id, user);}
 
-  // 어드민이 유저의 전체댓글 조회(admin입장에서 필요하다고 생각)
-//  @GetMapping("/users/{id}/comments")
-//  public List<CommentResponse> findRegisterComments() {
-//    return adminService.findRegisterComments();
-//  }
 
-  // 어드민 유저 전체 댓글 삭제....
-  //@DeleteMapping("/register/{id}/comments/delete")
-  //public List<CommentResponse> deleteRegisterComments(){return adminService.deleteRegisterComments();}
-
-  //order목록, 상품목록 추가 (관련 로직 설정 필요)
-  // @PutMapping("/order")
+  // 회원 role(일반,전문가,관리자) 분리하여 조회
+  @GetMapping("/users/role/{userRole}")
+  public Page<UserResponse> getUsersAllByRole(@PathVariable UserRole userRole){
+    return adminService.getUsersAllByRole(userRole);
+  }
 }
