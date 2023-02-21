c2fd28e04a329e932ab2c80b57c80ee5c43b52b6
taewoong kim <ktw2172@gmail.com>
2023-02-21 20:24:18 +0900

    FP147 [feat] 기프트카드 이미지 기능 추가, 회원가입 로그인 테스트 코드 추가

--- src/main/java/com/develonity/order/dto/GiftCardResponse.java
+++ src/main/java/com/develonity/order/dto/GiftCardResponse.java
@@ -10,47 +10,49 @@ import org.springframework.data.domain.Page;
 @Getter
 @NoArgsConstructor
 public class GiftCardResponse {
-    private GiftCardCategory category;
-    private Long id;
-    private String name;
-    private String details;
-    private String imageUrl;
-    private int price;
-    private int stockQuantity;
 
-    @Builder
-    public GiftCardResponse(GiftCardCategory category, Long id, String name, String details, String imageUrl, int price, int stockQuantity) {
-        this.category = category;
-        this.id = id;
-        this.name = name;
-        this.details = details;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.stockQuantity = stockQuantity;
-    }
+  private GiftCardCategory category;
+  private Long id;
+  private String name;
+  private String details;
+  private String imagePath;
+  private int price;
+  private int stockQuantity;
 
-    @Builder
-    public GiftCardResponse(GiftCard giftCard) {
-        this.category = giftCard.getCategory();
-        this.id = giftCard.getId();
-        this.name = giftCard.getName();
-        this.details = giftCard.getDetails();
-        this.imageUrl = giftCard.getImageUrl();
-        this.price = giftCard.getPrice();
-        this.stockQuantity = giftCard.getStockQuantity();
-    }
+  @Builder
+  public GiftCardResponse(GiftCardCategory category, Long id, String name, String details,
+      String imagePath, int price, int stockQuantity) {
+    this.category = category;
+    this.id = id;
+    this.name = name;
+    this.details = details;
+    this.imagePath = imagePath;
+    this.price = price;
+    this.stockQuantity = stockQuantity;
+  }
+
+  @Builder
+  public GiftCardResponse(GiftCard giftCard) {
+    this.category = giftCard.getCategory();
+    this.id = giftCard.getId();
+    this.name = giftCard.getName();
+    this.details = giftCard.getDetails();
+    this.imagePath = giftCard.getImagePath();
+    this.price = giftCard.getPrice();
+    this.stockQuantity = giftCard.getStockQuantity();
+  }
 
 
-    public Page<GiftCardResponse> toDtoList (Page<GiftCard> giftCardList) {
+  public Page<GiftCardResponse> toDtoList(Page<GiftCard> giftCardList) {
 
-        return giftCardList.map(m -> GiftCardResponse.builder()
-                .category(m.getCategory())
-                .id(m.getId())
-                .name(m.getName())
-                .details(m.getDetails())
-                .imageUrl(m.getImageUrl())
-                .price(m.getPrice())
-                .stockQuantity(m.getStockQuantity())
-                .build());
-    }
+    return giftCardList.map(m -> GiftCardResponse.builder()
+        .category(m.getCategory())
+        .id(m.getId())
+        .name(m.getName())
+        .details(m.getDetails())
+        .imagePath(m.getImagePath())
+        .price(m.getPrice())
+        .stockQuantity(m.getStockQuantity())
+        .build());
+  }
 }
--- src/main/java/com/develonity/order/entity/GiftCard.java
+++ src/main/java/com/develonity/order/entity/GiftCard.java
@@ -2,13 +2,17 @@ package com.develonity.order.entity;
 
 import com.develonity.common.exception.CustomException;
 import com.develonity.common.exception.ExceptionStatus;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
-
-import javax.persistence.*;
 
 @Entity
 @Getter
@@ -16,60 +20,66 @@ import javax.persistence.*;
 @DynamicUpdate
 public class GiftCard {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "GIFTCARD_ID")
-    private Long id;
-    @Enumerated(EnumType.STRING)
-    @Column(name = "category", nullable = false)
-    private GiftCardCategory category;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "GIFT_CARD_ID")
+  private Long id;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "category", nullable = false)
+  private GiftCardCategory category;
 
-    @Column(nullable = false)
-    private String name;
-    @Column(nullable = false)
-    private String details;
-    @Column(nullable = false)
-    private String imageUrl;
-    @Column(nullable = false)
-    private int price;
-    @Column(nullable = false)
-    private int stockQuantity; // 동시성 문제를 막기 위한 수량 설정(?)
+  @Column(nullable = false)
+  private String name;
+  @Column(nullable = false)
+  private String details;
+  @Column
+  private String imagePath;
+  @Column(nullable = false)
+  private int price;
+  @Column(nullable = false)
+  private int stockQuantity; // 동시성 문제를 막기 위한 수량 설정(?)
 
-    @Builder
-    public GiftCard(GiftCardCategory category, String name, String details, String imageUrl, int price, int stockQuantity) {
-        this.category = category;
-        this.name = name;
-        this.details = details;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.stockQuantity = stockQuantity;
+  @Builder
+  public GiftCard(GiftCardCategory category, String name, String details, String imagePath,
+      int price, int stockQuantity) {
+    this.category = category;
+    this.name = name;
+    this.details = details;
+    this.imagePath = imagePath;
+    this.price = price;
+    this.stockQuantity = stockQuantity;
+  }
+
+  public void update(GiftCardCategory category, String name, String details, String imagePath,
+      int price, int stockQuantity) {
+    this.category = category;
+    this.name = name;
+    this.details = details;
+    this.imagePath = imagePath;
+    this.price = price;
+    this.stockQuantity = stockQuantity;
+  }
+
+  /**
+   * stock 증가
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * stock 감소
+   */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+      throw new CustomException(ExceptionStatus.QUANTITY_IS_LACKING);
     }
+    this.stockQuantity = restStock;
+  }
 
-    public void update(GiftCardCategory category, String name, String details, String imageUrl, int price, int stockQuantity) {
-        this.category = category;
-        this.name = name;
-        this.details = details;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.stockQuantity = stockQuantity;
-    }
-
-    /**
-     * stock 증가
-     */
-    public void addStock(int quantity) {
-        this.stockQuantity += quantity;
-    }
-
-    /**
-     * stock 감소
-     */
-    public void removeStock(int quantity) {
-        int restStock = this.stockQuantity - quantity;
-        if (restStock < 0) {
-            throw new CustomException(ExceptionStatus.QUANTITY_IS_LACKING);
-        }
-        this.stockQuantity = restStock;
-    }
+  public void addImagePath(String imagePath) {
+    this.imagePath = imagePath;
+  }
 
 }
