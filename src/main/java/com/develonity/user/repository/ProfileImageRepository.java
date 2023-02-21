7f3c88c563165a68cd5a908a04b38fcfb8ef277b
taewoong kim <ktw2172@gmail.com>
2023-02-21 11:24:36 +0900

    FP142 [feat] 유저 프로필 기능 수정

--- src/main/java/com/develonity/user/repository/ProfileImageRepository.java
+++ src/main/java/com/develonity/user/repository/ProfileImageRepository.java
@@ -2,12 +2,15 @@ package com.develonity.user.repository;
 
 
 import com.develonity.user.entity.ProfileImage;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
 
 
-  ProfileImage findByUserId(Long userId);
+  Optional<ProfileImage> findByUserId(Long userId);
+
+//  ProfileImage findProfileImageByUserId(Long userId);
 
   void deleteByUserId(Long userId);
 
