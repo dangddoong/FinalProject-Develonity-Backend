6bf32bf5ebc2ac564adefd519abadbe6e0d6c512
taewoong kim <ktw2172@gmail.com>
2023-02-27 10:10:42 +0900

    FP 166 [feat] 로그인 테스트코드 위해 EmbeddedRedis 추가

--- src/main/java/com/develonity/board/controller/BoardController.java
+++ src/main/java/com/develonity/board/controller/BoardController.java
@@ -22,8 +22,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -130,7 +139,7 @@ public class BoardController {
       @AuthenticationPrincipal UserDetailsImpl userDetails,
       BoardPage questionBoardPage
   ) {
-    return questionBoardService.getQuetionBoardPage(userDetails.getUser(), questionBoardPage);
+    return questionBoardService.getQuestionBoardPage(userDetails.getUser(), questionBoardPage);
   }
 
 
@@ -140,7 +149,7 @@ public class BoardController {
       @AuthenticationPrincipal UserDetailsImpl userDetails,
       BoardPage questionBoardPage
   ) {
-    return questionBoardService.getTestQuetionBoardPage(userDetails.getUser(), questionBoardPage);
+    return questionBoardService.getTestQuestionBoardPage(userDetails.getUser(), questionBoardPage);
   }
 
   //잡담게시글 전체 조회(테스트용)
