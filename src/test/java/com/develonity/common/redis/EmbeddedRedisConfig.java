3e1950ac0fd53527f87f20729d043e3fab5f26d7
dangddoong <116885047+dangddoong@users.noreply.github.com>
2023-03-03 11:32:39 +0900

    [HOTFIX] CI TEST - 로그인테스트 주석처리

--- src/test/java/com/develonity/common/redis/EmbeddedRedisConfig.java
+++ src/test/java/com/develonity/common/redis/EmbeddedRedisConfig.java
@@ -18,7 +18,10 @@ class EmbeddedRedisConfig {
 
   @PostConstruct
   public void redisServer() throws IOException {
-    redisServer = new RedisServer(redisPort);
+    redisServer = RedisServer.builder()
+            .port(redisPort)
+            .setting("maxmemory 128M")
+            .build();
     redisServer.start();
   }
 
